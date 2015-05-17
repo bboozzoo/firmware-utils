3cad4e06aaa6a3afe0f5d14d95a0183ea1c67880
Maciek Borzecki <maciek.borzecki@gmail.com>
2015-05-17 21:23:23 +0200

    dir-locals: Emacs style file
    Place under .dir-locals.el name within kernel/module directory.

---
+++ dir-locals.el
@@ -0,0 +1,7 @@
+((c-mode . ((c-file-style . "linux")
+            (indent-tabs-mode . t)
+            (show-trailing-whitespace . t)
+            (c-offsets-alist . (arglist-cont-nonempty
+                                c-lineup-gcc-asm-reg
+                                c-lineup-arglist-tabs-only))))
+ )

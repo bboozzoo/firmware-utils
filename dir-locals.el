77b6693b27f6a17ecd414d64395d87c116fb7aaf
Maciek Borzecki <maciek.borzecki@gmail.com>
2015-05-21 23:50:19 +0200

    dir-locals.el: update dir locals to use linux-tabs-only style

--- dir-locals.el
+++ dir-locals.el
@@ -1,7 +1,7 @@
-((c-mode . ((c-file-style . "linux")
+((c-mode . ((c-file-style . "linux-tabs-only")
             (indent-tabs-mode . t)
             (show-trailing-whitespace . t)
-            (c-offsets-alist . (arglist-cont-nonempty
-                                c-lineup-gcc-asm-reg
-                                c-lineup-arglist-tabs-only))))
+            (c-basic-offset . 8)
+            (tab-width . 8)
+            ))
  )
